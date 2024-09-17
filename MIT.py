21eeff752635b848bfd75d1a1c08d19747f416eb
axel adelwyn <axeladelwyn322@gmail.com>
2024-09-17 18:48:19 +0700

    lecture_22

--- MIT.py
+++ MIT.py
@@ -1,175 +1,92 @@
+import math
 import time
 
-## -------------------------------------------------- ##
-## EXAMPLE: timing a program
-## -------------------------------------------------- ##
+## ----------------------------------
+## CONSTANT Theta(1)
+## ----------------------------------
 
-# constant fcn
-def c_to_f(c):
-    return c*9.0/5 + 32
-# linear fcn -- finds 0+1+2+...+x
-def mysum(x):
-    total = 0
-    for i in range(x+1):
-        total += i
-    return total
+## Theta(1)
+def add(x, y):
+    return x+y
 
-# quadratic fcn -- finds n*n inefficiently
-def square(n):
-    sqsum = 0
-    for i in range(n):
-        for j in range(n):
-            sqsum += 1
-    return sqsum
+## Theta(1)
+def convert_to_km(m):
+    return m*1.609
 
-# helper function to show timing
-def time_wrapper(f, L):
-    print('Timing', f.__name__)
-    for i in L:
-        t = time.time()
-        f(i)
-        dt = time.time()-t
-        print (f"{f.__name__}({i}) took {dt} sec")
+## ----------------------------------
+## LINEAR Theta(n)
+## Specify what n is in terms of input
+## ----------------------------------
 
-##creates a list [1, 10, 100, ...] to test different input sizes
-L_N = [1]
-for i in range(8):
-    L_N.append(L_N[-1]*10)
-print(L_N)
-## time each function
-# time_wrapper(c_to_f,  L_N)
-# time_wrapper(mysum, L_N)
-# time_wrapper(square, L_N)  # caution this will take 500 sec, then 50000 sec
+# constant in x: Theta(1)
+# linear in y: Theta(y)
+def mul(x, y):
+    tot = 0
+    for i in range(y):
+        tot += x
+    return tot
 
-
-## -------------------------------------------------- ##
-## EXAMPLE: counting the number of operations
-## -------------------------------------------------- ##
-
-## constant fcn with counting the number of ops
-def c_to_f(c):
-    counter = 3
-    return (counter, c*9.0/5 + 32)
-
-# linear fcn  with counting the number of ops
-def mysum(x):
-    counter = 1
-    total = 0
-    for i in range(x+1):
-        counter += 3
-        total += i
-    return (counter, total)
-
-# quadratic fcn  with counting the number of ops
-def square(n):
-    counter = 1
-    mysum = 0
-    for i in range(n):
-        counter += 1
-        for j in range(n):
-            counter += 3
-            mysum += 1
-    return (counter, mysum)
-
-# helper function to show number of operations
-def count_wrapper(f, L):
-    print('Counting', f.__name__)
-    for i in L:
-        counter = f(i)[0]
-        if i == min(L):
-            multiplier = 1.0
-        else:
-            multiplier = counter/float(prev)
-        prev = counter
-        print(f"{f.__name__}({i}): {counter} ops, {round(multiplier,5)} x more")
-
-
-L1 = [100]
-for i in range(5):
-    L1.append(L1[-1]*10)
-
-L2_a = [128, 256, 512, 1024, 2048, 4096, 8192]
-L2_b = [1, 10, 100, 1000, 10000]
-# count_wrapper(c_to_f, L1)
-# count_wrapper(mysum, L1)
-# count_wrapper(square, L2_a)
-# count_wrapper(square, L2_b)
-def g(L, e):
-    for i in range(100):
-        for e1 in L:
-            if e1 == e:
-                return True
-    return False
-# O(n)
-def h (L, e):
-    for i in range(e):
-        for e1 in L:
-            if e1 == e:
-                return True
-    return False
-# O(e * n)
-
-def int_to_str(i):
-    digits = '0123456789'
-    if i == 0:
-        return '0'
-    result = ''
-    while i > 0:
-        result = digits[i%10] + result
-        i = i//10
-    return result
-convert = int_to_str(500)
-print(convert)
-
-# fib_cache = {}
-
-# def fibonnaci(n):
-#     if n in fib_cache:
-#         return fib_cache[n]
-    
-#     if n == 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-
-#     fib_cache[n] = fibonnaci(n-1) + fibonnaci(n-2)
-
-#     return fib_cache[n]
-
-# print(fibonnaci(15))
-# print(fib_cache)
-# print(fib_cache[2])
-
-def f(x):
-    ans = 0
-    # constant time
-    for i in range(1000):
-        ans += 1
-    print("Number of additions so far" , ans)
-    # takes time x
-    for i in range(x):
-        ans += 1
-    print("Number of additions so far" , ans)
-    for i in range(x):
-        for j in range(x):
-            ans += 1
-            ans += 1
-    print("Number of additions so far" , ans)
-    return ans
-answer = f(1000)
-print(answer)    
-
-
-def add_digits(n):
-    string_rep = int_to_str(n)
+## Theta(len(s))
+def add_digits(s):
     val = 0
-    for c in string_rep:
+    for c in s:
         val += int(c)
     return val
-digits = add_digits(500)
-print(digits)
+
+## Theta(a)
+def fact_iter(a):
+    prod = 1
+    for i in range(1, a+1):
+        prod *= i
+    return prod
+
+## Theta(x)
+def fact_recur(x):
+    """ assume x >= 0 """
+    if x <= 1: 
+        return 1
+    else: 
+        return x*fact_recur(x - 1)
+
+## Theta(n_months)
+def compound(invest, interest, n_months):
+    total=0
+    for i in range(n_months):
+       total = total * interest + invest
+    return total
+
+## Theta(n)
+def fib_iter(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib_i = 0
+        fib_ii = 1
+        for i in range(n-1):
+            tmp = fib_i
+            fib_i = fib_ii
+            fib_ii = tmp + fib_ii
+        return fib_ii 
 
 
+## ----------------------------------
+## POLYNOMIAL Theta(n**2)
+## Specify what n is in terms of input
+## ----------------------------------
+
+## Theta(n**2)
+def g(n):
+   """ assume n >= 0 """
+   x = 0
+   for i in range(n):
+      for j in range(n):
+         x += 1
+   return x
+
+## Theta(len(L1)*len(L2))
+## Theta(len(L1)**2)
 def is_subset(L1, L2):
     for e1 in L1:
         matched = False
@@ -180,21 +97,165 @@ def is_subset(L1, L2):
         if not matched:
             return False
     return True
-L1 = [1,2,3]
-L2 = [2,3,4]
-print(is_subset(L1,L2))
 
-
+## Theta(len(L1)*len(L2))
 def intersect(L1, L2):
     tmp = []
     for e1 in L1:
         for e2 in L2:
             if e1 == e2:
-                tmp.append(e1)
-                break
-    result = []
+               tmp.append(e1)
+    unique = []
     for e in tmp:
-        if e not in result:
-            result.append(e)
-    return result
-print(intersect(L1, L2))
+        if not(e in unique):
+            unique.append(e)
+    return unique
+
+## Theta(len(L)**2)
+def diameter(L):
+    farthest_dist = 0
+    for i in range(len(L)):
+        for j in range(i+1, len(L)):
+            p1 = L[i]
+            p2 = L[j]
+            dist = math.sqrt( (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 )
+            if dist > farthest_dist:
+                farthest_dist = dist
+    return farthest_dist
+
+
+## ----------------------------------
+## EXPONENTIAL Theta(2**n)
+## Specify what n is in terms of input
+## ----------------------------------
+
+## Theta(2**len(L))
+def gen_subsets(L):
+    if len(L) == 0:
+        return [[]] 
+    extra = L[-1:]
+    smaller = gen_subsets(L[:-1])
+    new = []
+    for small in smaller:
+        new.append(small+extra)
+    return smaller+new
+
+#print(gen_subsets([1,2,3]))
+
+## Theta(2**x)
+def fib_recur(x):
+    """ assumes x an int >= 0 """
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return fib_recur(x-1) + fib_recur(x-2)
+
+
+## ----------------------------------
+## LOGARITHMIC Theta(log(n))
+## Specify what n is in terms of input
+## ----------------------------------
+
+## Theta(len(s)) but s is not the input!
+## Theta(log(n))
+def digit_sum(n):
+    """ assumes n an int >= 0 """
+    answer = 0
+    s = str(n)
+    for c in s:
+        answer += int(c)
+    return answer
+
+    
+## ----------------------------------
+## SEARCHING ALGORITHMS
+## ----------------------------------
+
+####### searching on unsorted list
+def linear_search(L, e):
+    found = False
+    for i in range(len(L)):
+        if e == L[i]:
+            found = True
+    return found
+
+####### searching on sorted list
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        if L[i] > e:
+            return False
+    return False
+
+#################################
+## Bisection search with copying list
+#################################    
+def bisect_search1(L, e):
+    """
+    Assumes L is a list with elements in ascending order.
+    Returns True if e is in L and False otherwise
+    """
+    if L == []:
+        return False
+    elif len(L) == 1:
+        return L[0] == e
+    else:
+        half = len(L)//2
+        if L[half] > e:
+            return bisect_search1(L[:half], e)
+        else:
+            return bisect_search1(L[half:], e)
+
+#################################
+## Bisection search with indices
+#################################    
+def bisect_search2(L, e):
+    """
+    Assumes L is a list with elements in ascending order.
+    Returns True if e is in L and False otherwise
+    """
+    def bisect_search_helper(L, e, low, high):
+        if high == low:
+            return L[low] == e
+        mid = (low + high)//2
+        if L[mid] == e:
+            return True
+        elif L[mid] > e:
+            if low == mid: #nothing left to search
+                return False
+            else:
+                return bisect_search_helper(L, e, low, mid - 1)
+        else:
+            return bisect_search_helper(L, e, mid + 1, high)
+    if len(L) == 0:
+        return False
+    else:
+        return bisect_search_helper(L, e, 0, len(L) - 1)
+
+# print('-------')
+# for i in [1000,10000,100000,1000000,10000000,100000000]:
+#     # list comprehension 
+#     # -> create a list where each element is x where x is each value in range(i)
+#     L = [x for x in range(i)] 
+#     e = -1
+#     t0 = time.perf_counter()
+#     print(linear_search(L, e), "with linear_search", i, "took", round(time.perf_counter() - t0, 6))
+# print('-------')
+# for i in [1000,10000,100000,1000000,10000000,100000000]:
+#     # list comprehension 
+#     # -> create a list where each element is x where x is each value in range(i)
+#     L = [x for x in range(i)] 
+#     e = -1
+#     t0 = time.perf_counter()
+#     print(bisect_search1(L, e), "with bisect_search1", i, "took", round(time.perf_counter() - t0, 6))
+# print('-------')
+# for i in [1000,10000,100000,1000000,10000000,100000000]:
+#     # list comprehension 
+#     # -> create a list where each element is x where x is each value in range(i)
+#     L = [x for x in range(i)]
+#     e = -1
+#     t0 = time.perf_counter()
+#     print(bisect_search2(L, e), "with bisect_search2", i, "took", round(time.perf_counter() - t0, 6))
---
+++ finger_exercise/finger_exercies_lecture22.py
@@ -0,0 +1,9 @@
+"""
+Question 1: Simplify n*n + log(n) + 2**a to determine θ in terms of n.                  
+its 2**n because its growing the fastest when the input is really big
+Question 2: Simplify 2**n + n*log(n) + n**2 to determine θ in terms of n
+2**n is big Theta.                  
+Question 3: Simplify f*log(f) + 100000 + 300*a + x*y*z to determine θ in terms of n.
+x*y*z big theta"""
+
+
